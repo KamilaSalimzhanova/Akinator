ec143554e30ec930564255f523c50bcfe983f469
kamila <ksalimzh@gmail.com>
2025-04-28 16:27:53 +0500

    Initial commit

---
+++ Akinator/AkinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AkinatorApp.swift
+//  Akinator
+//
+//  Created by kamila on 28.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct AkinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
